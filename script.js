263c778e6d74e0fb840d6a05c12ed00e2d5c5429
kircanski023 <kircanskis@gmail.com>
2025-03-31 14:35:26 +0200

    Add all basic functions for calculations
    Functions are for additon, substraction, multiplication and divison

---
+++ script.js
@@ -0,0 +1,21 @@
+let firstNumber = 0;
+const operator = "+-*/";
+let secondNumber = 0;
+
+function add(a, b){
+    return a + b;
+}
+
+function subtract(a, b){
+    return a - b;
+}
+
+function multiply(a, b){
+    return a * b;
+}
+
+function divide(a, b){
+    return a / b;
+}
+
+console.log(add(60, 3))
