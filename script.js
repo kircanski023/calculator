cb91b93d690204b69f809492a5c38775d8361832
kircanski023 <kircanskis@gmail.com>
2025-03-31 15:24:52 +0200

    Add function to operate based on input
    Created a function called operate() which returns a valid operation based on input

--- script.js
+++ script.js
@@ -18,4 +18,24 @@ function divide(a, b){
     return a / b;
 }
 
-console.log(add(60, 3))
+function operate(operator, a, b){
+    switch (operator) {
+        case "+":
+        return add(a, b)
+
+        case "-":
+        return subtract(a, b)
+
+        case "*":
+        return multiply(a, b)
+
+        case "/":
+        return divide(a, b)
+
+        default:
+            break;
+    }
+
+   
+}
+console.log(operate( "-" , 36, 2))
